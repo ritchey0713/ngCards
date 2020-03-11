9c18bc7a9dec51d8a00e424fa5abcd86ef0df571
Dj <ritchey0713@gmail.com>
2020-03-11 13:39:06 -0400

    add array of post objs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title: "Neat Tree",
+      username: "nature",
+      imageUrl: "assets/images/tree.jpeg",
+      content: "I saw a neat tree today"
+    },
+    {
+      title: "Snowy Mountain",
+      username: "mountain-lover",
+      imageUrl: "assets/images/mountain.jpeg",
+      content: "My fave mountain to climb!"
+    },
+    {
+      title: "Bike race",
+      username: "biker123",
+      imageUrl: "assets/images/biking.jpeg",
+      content: "Snapshot from the race today"
+    }
+  ]
 }
